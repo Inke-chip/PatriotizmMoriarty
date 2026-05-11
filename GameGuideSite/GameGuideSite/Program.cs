67563f30b8ca75d6d16f13dfafeee3a2d6c86d92
kirger06 <kirger06@gmail.com>
2026-05-11 23:22:15 +0300

    program.cs

--- GameGuideSite/GameGuideSite/Program.cs
+++ GameGuideSite/GameGuideSite/Program.cs
@@ -1,25 +1,24 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddRazorPages();
+// Добавляем поддержку MVC (контроллеры и представления)
+builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseExceptionHandler("/Home/Error");
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
 
-app.MapRazorPages();
+// Маршрут для MVC (а не Razor Pages)
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Games}/{action=Index}/{id?}");
 
 app.Run();
--- GameGuideSite/GameGuideSite/obj/Debug/net8.0/GameGuideSite.AssemblyInfo.cs
+++ GameGuideSite/GameGuideSite/obj/Debug/net8.0/GameGuideSite.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameGuideSite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+349a8d91bdf9f2d8ad4de6ffaa7237353be119ac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a6c44892e0c8612e0aed9b3977b5293a90a847a")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameGuideSite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameGuideSite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
